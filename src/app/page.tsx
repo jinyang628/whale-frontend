6289a10c428ee0882add2c64bd7f86c9517f38c5
jinyang628 <chenjinyang4192@gmail.com>
2024-07-23 22:03:57 -0700

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,6 @@ import { updateCacheRequestSchema } from "@/types/api/user/update-cache";
 import { updateCache } from "@/api/user/update-cache";
 import { getCache } from "@/api/user/get-cache";
 import Image from "next/image";
-import LoginButton from "@/components/authentication/login-button";
 
 export default function Home() {
   const [applicationContentArr, setApplicationContentArr] = useState<ApplicationContent[]>([])
