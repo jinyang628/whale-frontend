f92ff11af6e6b01b343bcb239b9ed09823867bce
aarontantf <aaron@tinyfish.io>
2024-07-26 13:18:08 -0700

    script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import {
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme/theme-provider";
 import Script from "next/script";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,7 +23,9 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-      <Script src="https://cloud.umami.is/script.js" data-website-id="293934de-1d90-48b8-8d99-d72a76aa488a" />
+        <Head>
+          <Script async src="https://cloud.umami.is/script.js" data-website-id="293934de-1d90-48b8-8d99-d72a76aa488a" />
+        </Head>
         <body className={inter.className}>
           <ThemeProvider 
               attribute="class"
